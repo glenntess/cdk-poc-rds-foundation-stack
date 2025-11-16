83efe13dd6ede29fd04a6e3cd37f23782dd46c67
Glenn <glenn.tesoriero@outlook.com>
2025-11-17 08:33:55 +1100

    Initial commit

---
+++ src/CdkFoundation/CdkFoundationStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace CdkFoundation
+{
+    public class CdkFoundationStack : Stack
+    {
+        internal CdkFoundationStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
