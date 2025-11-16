fd056c3f4453090d49dc902e7d74627810ff38fa
Glenn <glenn.tesoriero@outlook.com>
2025-11-17 08:50:49 +1100

    Created app

--- src/CdkFoundation/CdkFoundationStack.cs
+++ src/CdkFoundation/CdkFoundationStack.cs
@@ -1,5 +1,8 @@
 using Amazon.CDK;
 using Constructs;
+using Amazon.CDK.AWS.KMS;
+using Amazon.CDK.AWS.S3;
+using Amazon.CDK.AWS.CloudTrail;
 
 namespace CdkFoundation
 {
@@ -7,7 +10,28 @@ namespace CdkFoundation
     {
         internal CdkFoundationStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
-            // The code that defines your stack goes here
+            // KMS Key
+            var kmsKey = new Key(this, "BaselineRdsKmsKey", new KeyProps {
+                Alias = "alias/glt-baseline-rds",
+                EnableKeyRotation = true,
+                Description = "KMS key for RDS encryption and Secrets Manager"
+            });
+
+            // S3 Bucket for CloudTrail
+            var trailBucket = new Bucket(this, "SecOpsCloudTrailBucket", new BucketProps {
+                BucketName = "glt-secops-cloudtrail",
+                BlockPublicAccess = BlockPublicAccess.BLOCK_ALL,
+                RemovalPolicy = RemovalPolicy.DESTROY
+            });
+
+            // CloudTrail
+            new Trail(this, "SecOpsCloudTrail", new TrailProps {
+                TrailName = "glt-baseline-cloudtrail-secops",
+                Bucket = trailBucket,
+                IsMultiRegionTrail = true,
+                IncludeGlobalServiceEvents = true,
+                EnableFileValidation = true
+            });
         }
     }
 }
